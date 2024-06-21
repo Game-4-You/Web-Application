6aa920a1a4c24ff9cb0fc7beca1c22d23d7803ad
JuanPescoran <u20221c936@upc.edu.pe>
2024-06-21 00:32:43 -0500

    feat(templates): added recommendations for home page

--- Página-Web/index.py
+++ Página-Web/index.py
@@ -2,6 +2,7 @@ import os
 import json
 from flask import Flask, render_template, request, redirect, url_for, jsonify, session
 import secrets
+import requests
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
@@ -10,6 +11,32 @@ data_file_path = os.path.join(os.path.dirname(__file__), 'server', 'data.json')
 
 with open(data_file_path) as f:
     users = json.load(f)
+
+
+# Define la función para obtener la URL de la imagen
+def get_image_url(game_name):
+    api_key = 'AIzaSyBeGyYTzIqeHxvWU7SVWKOrZ-sueLe9ho0'
+    search_engine_id = '763dfea0c13cf4b9a'
+    search_url = f"https://www.googleapis.com/customsearch/v1?q={game_name}&searchType=image&key={api_key}&cx={search_engine_id}"
+
+    response = requests.get(search_url)
+    results = response.json()
+
+    if 'items' in results and len(results['items']) > 0:
+        return results['items'][0]['link']
+    else:
+        # URL de imagen predeterminada local
+        return url_for('static', filename='assets/default_image.jpg')  # Asegúrate de tener esta imagen en la carpeta 'static/assets'
+
+# Nueva función para obtener los juegos de los amigos
+def get_friends_games(user):
+    friends_games = []
+    for friend_id in user['Friends']:
+        for friend in users:
+            if friend['UserID'] == friend_id:
+                friends_games.append({'Username': friend['Username'], 'Games': friend['Games']})
+                break
+    return friends_games
 
 
 @app.route('/data')
@@ -28,8 +55,21 @@ def initial():
 def home():
     if 'user' not in session:
         return redirect(url_for('login'))
+
     user = session['user']
-    return render_template('home.html', user=user)
+
+    # Agrega la URL de la imagen a cada juego
+    for game in user['Games']:
+        game['ImageUrl'] = get_image_url(game['Name'])
+
+    # Obtener los juegos de los amigos
+    friends_games = get_friends_games(user)
+
+    for friend in friends_games:
+        for game in friend['Games']:
+            game['ImageUrl'] = get_image_url(game['Name'])
+
+    return render_template('home.html', user=user, friends_games=friends_games)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -78,6 +118,11 @@ def free():
 @app.route('/subscription')
 def subscription():
     return render_template('subscription.html')
+
+
+@app.route('/register2')
+def register2():
+    return render_template('register2.html')
 
 
 if __name__ == '__main__':
--- Página-Web/static/sidebar.js
+++ Página-Web/static/sidebar.js
@@ -1,22 +1,22 @@
-window.onload = function() {
+window.onload = function () {
     var sidebarToggleOff = document.getElementById('sidebarToggleOff');
     var sidebarToggleOn = document.getElementById('sidebarToggleOn');
     var sidebar = document.getElementById('Sidebar');
 
     if (sidebarToggleOff && sidebarToggleOn && sidebar) {
-        sidebarToggleOn.addEventListener('click', function() {
+        sidebarToggleOn.addEventListener('click', function () {
             console.log('El botón sidebarToggleOn ha sido presionado.');
             sidebar.style.display = 'flex';
         });
 
-        sidebarToggleOff.addEventListener('click', function() {
+        sidebarToggleOff.addEventListener('click', function () {
             console.log('El botón sidebarToggleOff ha sido presionado.');
             sidebar.style.display = 'none';
         });
 
         var sidebarButtons = sidebar.getElementsByTagName('button');
         for (var i = 0; i < sidebarButtons.length; i++) {
-            sidebarButtons[i].addEventListener('click', function() {
+            sidebarButtons[i].addEventListener('click', function () {
                 console.log('El botón ' + this.innerText + ' ha sido presionado.');
                 switch (this.id) {
                     case 'Inicio':
